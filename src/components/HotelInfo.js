44392b8595e464fc0f917e6f837a70d6b1e6d4bd
Krushanu Mohapatra <mohapatrakrushanu@gmail.com>
2023-06-06 04:51:11 -0400

    Updated Hotel Info UI

--- src/components/HotelInfo.js
+++ src/components/HotelInfo.js
@@ -31,19 +31,6 @@ const HotelInfo = () => {
           <p>We're committed to maintaining the same quality of service for every individual. We offer the following facilities for those with special needs:</p>
           <ul>
             {accessibilities.map(eachAccessibility => <li>{eachAccessibility.name}</li>)}
-            <li>Grab bars on tub walls</li>
-            <li>Shower chairs</li>
-            <li>Hand held shower sprayers</li>
-            <li>Higher toilets &amp; toilet modifiers</li>
-            <li>Lower sink faucet handles</li>
-            <li>Wheelchair clearance under sinks &amp; vanity</li>
-            <li>Lower racks in closet</li>
-            <li>TDD machines</li>
-            <li>Telephone light signalers  &amp; smoke alarms</li>
-            <li>Telephone amplification handsets</li>
-            <li>Closed captioned television converters</li>
-            <li>Vibrating alarm clocks</li>
-            <li>Telephones with volume control</li>
           </ul>
         </section>
       </article>
